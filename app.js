129786fa44f1dec8ff496a6918a5a869f9673beb
Itzhak07 <itzhak07@gmail.com>
2020-03-06 22:06:40 +0200

    deploy

--- app.js
+++ app.js
@@ -50,9 +50,9 @@ app.use("/api/workouts", workoutsRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 // catch 404 and forward to error handler
