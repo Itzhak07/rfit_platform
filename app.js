7757da36f51d69f2d341d8f75529ad3c4efd39d5
Itzhak07 <itzhak07@gmail.com>
2020-03-06 22:10:29 +0200

    deploy

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use("/api/clients", clientsRouter);
 app.use("/api/workouts", workoutsRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("./client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
