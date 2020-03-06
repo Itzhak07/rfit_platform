8b42b50cb83cc153d2340916ad0a0f9d45c168ae
Itzhak07 <itzhak07@gmail.com>
2020-03-06 23:12:14 +0200

    deploy

--- app.js
+++ app.js
@@ -56,8 +56,9 @@ app.use(function(req, res, next) {
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
