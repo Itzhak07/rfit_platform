c3f27685e104c1a4d63f299516fcf2ea779feaaf
Itzhak07 <itzhak07@gmail.com>
2020-03-06 22:52:24 +0200

    deploy

--- app.js
+++ app.js
@@ -55,10 +55,10 @@ app.use(function(req, res, next) {
 // heroku check
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static(path.join(__dirname, './client/build')));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './client/build/', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
