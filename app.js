e9ea343ee79728246a47810e4d5a8aa05b259633
Itzhak07 <itzhak07@gmail.com>
2020-03-07 16:36:54 +0200

    deploy

--- app.js
+++ app.js
@@ -54,10 +54,10 @@ app.use("/api/workouts", workoutsRouter);
 // heroku check
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static('client/build'));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "../client/build/index.html"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
