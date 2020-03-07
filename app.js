ad79650d1681c493407c298c769f6cff5085ce80
Itzhak07 <itzhak07@gmail.com>
2020-03-07 16:19:58 +0200

    deploy

--- app.js
+++ app.js
@@ -39,9 +39,9 @@ if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // });
 }
 
 // view engine setup
