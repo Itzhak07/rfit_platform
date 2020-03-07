84ecc5a19ad7f4f6c060672cc8ca3fc4eda1a26e
Itzhak07 <itzhak07@gmail.com>
2020-03-07 16:32:45 +0200

    deploy

--- app.js
+++ app.js
@@ -67,14 +67,14 @@ if (process.env.NODE_ENV === "production") {
 // });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.json("error");
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.json("error");
+// });
 
 module.exports = app;
