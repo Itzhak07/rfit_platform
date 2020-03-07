93ea25647f4281dce93628dad21fdee6c10595d5
Itzhak07 <itzhak07@gmail.com>
2020-03-07 17:37:53 +0200

    deploy

--- server.js
+++ server.js
@@ -38,12 +38,12 @@ app.use("/api/clients", clientsRouter);
 app.use("/api/workouts", workoutsRouter);
 
 // Serve static assets in production
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static(path.join("client/build")));
+  app.use(express.static('client/build'));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve("./client/build/index.html"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
