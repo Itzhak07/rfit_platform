043857678a7fc32c21104d32aee7088ff5ebc227
Itzhak07 <itzhak07@gmail.com>
2020-03-19 19:35:22 +0200

    cardsHeader / add border-radius

--- client/src/layouts/Client/ClientProfilePage.js
+++ client/src/layouts/Client/ClientProfilePage.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
 import ClientWorkoutsTable from "../../components/Client/ClientWorkoutsTable";
-import { Paper, makeStyles } from "@material-ui/core";
+import { Paper, makeStyles, Container } from "@material-ui/core";
 import { ClientProfile } from "../../components/Client/ClientProfile";
 import MenuButton from "../../components/Buttons/MenuButton";
 import { ClientBreadCrumbs } from "./ClientBreadCrumbs";
@@ -17,11 +17,11 @@ const useStyles = makeStyles({
   info: {
     padding: 20,
     maxWidth: 500,
-    width: "100%"
+ 
   },
   workouts: {
     padding: 20,
-    width: "100%",
+    // width: "100%",
     marginTop: 20
   },
   item: { padding: "0 0 20px 0" }
@@ -52,15 +52,17 @@ function ClientProfilePage({ clients, workouts }) {
       <ClientBreadCrumbs
         clientName={thisClient.firstName + " " + thisClient.lastName}
       />
-      <Paper className={classes.info} variant="outlined">
-        {<ClientProfile client={thisClient} />}
-      </Paper>
-      <Paper className={classes.workouts} variant="outlined">
-        <ClientWorkoutsTable workouts={thisWorkouts} client={thisClient} />
-      </Paper>
-      <div>
-        <MenuButton editClient />
-      </div>
+      <Container maxWidth="xl" disableGutters>
+        <Paper className={classes.info} variant="outlined">
+          {<ClientProfile client={thisClient} />}
+        </Paper>
+        <Paper className={classes.workouts} variant="outlined">
+          <ClientWorkoutsTable workouts={thisWorkouts} client={thisClient} />
+        </Paper>
+        <div>
+          <MenuButton editClient />
+        </div>
+      </Container>
     </div>
   );
 }
