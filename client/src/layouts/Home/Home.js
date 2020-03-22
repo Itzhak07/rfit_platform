54550a246267776e3ac8051c9f9a03a072ece8ef
Itzhak07 <itzhak07@gmail.com>
2020-03-22 17:14:17 +0200

    LandingPage/ Home / fix responsive bugs

--- client/src/layouts/Home/Home.js
+++ client/src/layouts/Home/Home.js
@@ -5,39 +5,47 @@ import MenuButton from "../../components/Buttons/MenuButton";
 import CardsHeader from "./CardsHeader";
 import TopClients from "../../components/Client/TopClients";
 import { FastLinks } from "./FastLinks";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    flexFlow: "row-reverse wrap",
+    justifyContent: "space-evenly",
+    background: `url(${bgwhite})`
+  },
+  leftSection: {
+    display: "flex"
+  },
+  rightSection: {
+    marginBottom: 20,
+    transition: "all 0.3s ease",
+    [theme.breakpoints.only("lg")]: {
+      maxWidth: 650,
+      transition: "all 0.3s ease"
+    }
+  },
+  rightBottom: {
+    display: "flex",
+    flexFlow: "row wrap",
+    justifyContent: "space-around"
+  }
+}));
 
 const Home = () => {
-  const styles = {
-    root: {
-      display: "flex",
-      flexFlow: "row-reverse wrap",
-      justifyContent: "space-evenly",
-      background: `url(${bgwhite})`
-    },
-    leftSection: {
-      display: "flex"
-    },
-    rightSection: {
-      marginBottom: 20
-    },
-    rightBottom: {
-      display: "flex",
-      flexFlow: "row wrap",
-      justifyContent: "space-around"
-    }
-  };
+  const classes = useStyles();
 
   return (
-    <div style={styles.root}>
-      <div style={styles.rightSection}>
+    <div className={classes.root}>
+      <div className={classes.rightSection}>
         <CardsHeader />
-        <div style={styles.rightBottom}>
+        <div className={classes.rightBottom}>
           <FastLinks />
           <TopClients />
         </div>
       </div>
 
-      <div style={styles.leftSection}>
+      <div className={classes.leftSection}>
         <DaySchedule />
       </div>
       <MenuButton />
